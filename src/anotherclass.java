71961b5aff4983ab0fc1fc32317b21689ee97ad7
Mustafagenccc <mstfgncc@gmail.com>
2025-07-07 22:27:42 +0300

    t8

--- src/anotherclass.java
+++ src/anotherclass.java
@@ -1,4 +1,6 @@
 public class anotherclass {
     //techpro bracnh,version t7
     public String string1 = "Biri sen, biri ben, iki damla yas akti gozlerimden";
+    //techpro branch, t8
+    public String string2 = "Parildayıp duran insanlara bak";
 }
